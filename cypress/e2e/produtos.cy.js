fe7d00369d7693f4fc07ff303019e9c77457ce0d
herodmfrancato <francatoherod@gmail.com>
2025-04-23 21:35:01 -0300

    Adicionando teste de remocao de produto do carrinho

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -13,4 +13,21 @@ describe('Produtos', () => {
         cy.get('.links_Product_areas > .theme-btn-one').click()
         cy.get('#swal2-title').should('be.visible')
       });
+
+      it('Procurando e adicionando produto no carrinho', () => {
+        cy.login('herod@qazando.com.br', '123456')
+        cy.get('.swal2-confirm').click()
+        cy.get(':nth-child(1) > .search_width > img').click()
+        cy.get('form > input').type('Monitor')
+        cy.get('.btn').click()
+        cy.get('#swal2-title').should('be.visible')
+        cy.get('.swal2-confirm').click()
+        cy.get(':nth-child(1) > .product_wrappers_one > .content > .title > a').click()
+        cy.get('.links_Product_areas > .theme-btn-one').click()
+        cy.get('#swal2-title').should('be.visible')
+        cy.get(':nth-child(3) > .offcanvas-toggle > .fa').click()
+        cy.get('.offcanvas-cart > :nth-child(4) > .text-right > .offcanvas-wishlist-item-delete > .fa').click()
+      });
+
+      
 });
